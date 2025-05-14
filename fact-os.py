2c06dd183da595d2fa5415f0b19d73a8eec488b7
mlopez <ml.3012@gmail.com>
2025-05-14 15:30:42 -0300

    year filter

--- fact-os.py
+++ fact-os.py
@@ -32,42 +32,17 @@ conn = mysql.connector.connect(
     database=os.getenv("DB_NAME")
 )
 
-# Ejecutar la query
-query = """
-SELECT o.os_nombre AS obra_social, COUNT(p.prestacion_id) AS cantidad_prestaciones
-FROM v_prestaciones p JOIN v_os o 
-ON p.prestacion_os = o.os_id
-WHERE prestacion_estado_descrip = "ACTIVA" COLLATE utf8mb4_0900_ai_ci
-GROUP BY obra_social
-"""
-df = pd.read_sql(query, conn)
+# Filtro de Año
+year = st.selectbox("Seleccione el año", ["Todos", "2024", "2025"])
 
-# Asegurar orden correcto
-df = df.sort_values('cantidad_prestaciones', ascending=False)
-
-# Gráfico
-fig = px.bar(
-    df,
-    x='obra_social',
-    y='cantidad_prestaciones',
-    title='Cantidad de prestaciones por obra social',
-    labels={'obra_social': 'Obra Social', 'cantidad_prestaciones': 'Cantidad'},
-    text='cantidad_prestaciones'
-)
-
-# Ajustar layout para que se use todo el ancho
-fig.update_layout(
-    title_x=0.5,
-    margin=dict(l=00, r=0, t=40, b=20),
-    height=600
-)
-
-# Mostrar en Streamlit
-st.plotly_chart(fig, use_container_width=False)
+# Condición de año en la consulta
+year_condition = ""
+if year != "Todos":
+    year_condition = f"AND YEAR(c.cbteFch) = {year}"
 
 # Tarjeta de promedio total de dif. de días
 
-query3 = """ 
+q_prom_card = f""" 
     SELECT 
         ROUND(AVG(DATEDIFF(c.cobro_fec, c.cbteFch)), 2) AS promedio_total
     FROM 
@@ -76,22 +51,22 @@ query3 = """
     WHERE 
         c.cbteFch IS NOT NULL
         AND c.cobro_fec IS NOT NULL
-        AND o.os_nombre NOT IN ('OSDE');
+        AND o.os_nombre NOT IN ('OSDE')
+        {year_condition};
 """
 
-df3 = pd.read_sql(query3, conn)
+df_prom_card = pd.read_sql(q_prom_card, conn)
 
-prom_total = df3['promedio_total'][0]
+prom_total = df_prom_card['promedio_total'][0]
 
-st.markdown("<div class='space'></div>", unsafe_allow_html=True)
 
 # Mostrar en tarjeta
 
 st.markdown(f"""
     <div class="card-container">
         <div class="card">
-            <div class="card-title">Promedio total de días de pago</div>
-            <div class="card-value">{prom_total} %</div>
+            <div class="card-title">Promedio total de días de pago ({year})</div>
+            <div class="card-value">{prom_total}</div>
         </div>
     </div>
 """, unsafe_allow_html=True)
@@ -100,7 +75,7 @@ st.markdown("<div class='space'></div>", unsafe_allow_html=True)
 
 # Gráfico de días para autorizar por os
 
-query2 = """
+q_days_aut = f"""
 SELECT 
     o.os_nombre AS obra_social,
     ROUND(AVG(DATEDIFF(c.cobro_fec, c.cbteFch)), 2) AS promedio_dias
@@ -111,34 +86,70 @@ WHERE
     c.cbteFch IS NOT NULL
     AND c.cobro_fec IS NOT NULL
     AND o.os_nombre NOT IN ('OSDE')
+    {year_condition}
 GROUP BY obra_social
 HAVING 
     promedio_dias IS NOT NULL
 """
 
-df2 = pd.read_sql(query2, conn)
+df_days_aut = pd.read_sql(q_days_aut, conn)
 
-conn.close()
+df_days_aut = df_days_aut.sort_values(by='promedio_dias', ascending=True)
 
-df2 = df2.sort_values(by='promedio_dias', ascending=True)
-
-# st.subheader("Promedio de días de pago")
-
-fig2 = px.bar(
-    df2,
+fig_days_aut = px.bar(
+    df_days_aut,
     x="obra_social",
     y="promedio_dias",
-    title="Promedio de días de pago por obra social",
+    title=f"Promedio de días de pago por obra social ({year})",
     labels={"obra_social": "Obra Social", "promedio_dias": "Días promedio"},
     text='promedio_dias'
 )
-fig2.update_layout(
+
+fig_days_aut.update_layout(
     title_x=0.5,
-    margin=dict(l=0, r=0, t=40, b=20),
     height=600
 )
 
-st.plotly_chart(fig2, use_container_width=False)
+
+st.plotly_chart(fig_days_aut, use_container_width=False)
+
+st.markdown("<div class='space'></div>", unsafe_allow_html=True)
+
+
+# Cant. de prestaciones por OS
+
+q_prest_os = """
+SELECT o.os_nombre AS obra_social, COUNT(p.prestacion_id) AS cantidad_prestaciones
+FROM v_prestaciones p JOIN v_os o 
+ON p.prestacion_os = o.os_id
+WHERE prestacion_estado_descrip = "ACTIVA" COLLATE utf8mb4_0900_ai_ci
+GROUP BY obra_social
+"""
+df_prest_os = pd.read_sql(q_prest_os, conn)
+
+conn.close()
+
+# Asegurar orden correcto
+df_prest_os = df_prest_os.sort_values('cantidad_prestaciones', ascending=False)
+
+# Gráfico
+fig_prest_os = px.bar(
+    df_prest_os,
+    x='obra_social',
+    y='cantidad_prestaciones',
+    title='Cantidad de prestaciones por obra social',
+    labels={'obra_social': 'Obra Social', 'cantidad_prestaciones': 'Cantidad'},
+    text='cantidad_prestaciones'
+)
+
+# Ajustar layout para que se use todo el ancho
+fig_prest_os.update_layout(
+    title_x=0.5,
+    height=600
+)
+
+# Mostrar en Streamlit
+st.plotly_chart(fig_prest_os, use_container_width=False)
 
 
 
